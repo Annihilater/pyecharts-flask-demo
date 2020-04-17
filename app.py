da881c030c43afee3fca2bdec9d70970dc45b097
Annihilater <yanmiexingkong@gmail.com>
2020-04-17 17:39:13 +0800

    feat: 新增定时增量更新功能

--- app.py
+++ app.py
@@ -8,23 +8,32 @@
 # 前后端分离
 from random import randrange
 
+from flask.json import jsonify
 from flask import Flask, render_template
 
 from pyecharts import options as opts
-from pyecharts.charts import Bar
+from pyecharts.charts import Line
 
 app = Flask(__name__, static_folder="templates")
 
 
-def bar_base() -> Bar:
-    c = (
-        Bar()
-            .add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
-            .add_yaxis("商家A", [randrange(0, 100) for _ in range(6)])
-            .add_yaxis("商家B", [randrange(0, 100) for _ in range(6)])
-            .set_global_opts(title_opts=opts.TitleOpts(title="Bar-基本示例", subtitle="我是副标题"))
+def line_base() -> Line:
+    line = (
+        Line()
+            .add_xaxis(["{}".format(i) for i in range(10)])
+            .add_yaxis(
+            series_name="",
+            y_axis=[randrange(50, 80) for _ in range(10)],
+            is_smooth=True,
+            label_opts=opts.LabelOpts(is_show=False),
+        )
+            .set_global_opts(
+            title_opts=opts.TitleOpts(title="动态数据"),
+            xaxis_opts=opts.AxisOpts(type_="value"),
+            yaxis_opts=opts.AxisOpts(type_="value"),
+        )
     )
-    return c
+    return line
 
 
 @app.route("/")
@@ -32,10 +41,20 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/barChart")
-def get_bar_chart():
-    c = bar_base()
+@app.route("/lineChart")
+def get_line_chart():
+    c = line_base()
     return c.dump_options_with_quotes()
+
+
+idx = 9
+
+
+@app.route("/lineDynamicData")
+def update_line_data():
+    global idx
+    idx = idx + 1
+    return jsonify({"name": idx, "value": randrange(50, 80)})
 
 
 if __name__ == "__main__":
